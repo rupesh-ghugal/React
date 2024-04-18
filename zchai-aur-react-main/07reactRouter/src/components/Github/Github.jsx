26cc6ecb54e1aa745f3de106644d73ef96bb5ded
Rupesh <rupeshghugal1917@gmail.com>
2024-04-18 09:54:00 +0530

    project 10 commit

--- zchai-aur-react-main/07reactRouter/src/components/Github/Github.jsx
+++ zchai-aur-react-main/07reactRouter/src/components/Github/Github.jsx
@@ -13,7 +13,7 @@ function Github() {
     //  })
     // }, [])
     
-  return (
+  return (  
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
     <img src={data.avatar_url} alt="Git picture" width={300} />
     </div>
